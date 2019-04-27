1337057f9bf2b38a09c335a31257c7ce8d09facf
Aditya_P <adityapednekar@live.com>
2019-04-27 15:05:12 +0530

    Latest version

--- php/form-process.php
+++ php/form-process.php
@@ -1,11 +1,14 @@
 <?php
+
 $errorMSG = "";
+
 // NAME
 if (empty($_POST["name"])) {
     $errorMSG = "Name is required ";
 } else {
     $name = $_POST["name"];
 }
+
 // EMAIL
 if (empty($_POST["email"])) {
     $errorMSG .= "Email is required ";
@@ -13,20 +16,17 @@ if (empty($_POST["email"])) {
     $email = $_POST["email"];
 }
 
-// msg_subject
-if (empty($_POST["msg_subject"])) {
-    $errorMSG .= "Subject is required ";
-} else {
-    $email = $_POST["msg_subject"];
-}
 // MESSAGE
 if (empty($_POST["message"])) {
     $errorMSG .= "Message is required ";
 } else {
     $message = $_POST["message"];
 }
-$EmailTo = "measuresinfinite89@gmail.com";
+
+
+$EmailTo = "work@howimage.com";
 $Subject = "New Message Received";
+
 // prepare email body text
 $Body = "";
 $Body .= "Name: ";
@@ -35,14 +35,13 @@ $Body .= "\n";
 $Body .= "Email: ";
 $Body .= $email;
 $Body .= "\n";
-$Body .= "msg_subject: ";
-$Body .= $msg_subject;
-$Body .= "\n";
 $Body .= "Message: ";
 $Body .= $message;
 $Body .= "\n";
+
 // send email
-$success = mail($EmailTo, $msg_subject, $Body, "From:".$email);
+$success = mail($EmailTo, $Subject, $Body, "From:".$email);
+
 // redirect to success page
 if ($success && $errorMSG == ""){
    echo "success";
@@ -53,4 +52,5 @@ if ($success && $errorMSG == ""){
         echo $errorMSG;
     }
 }
+
 ?>
